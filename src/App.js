e53b6d62fa3a3ca6bd6a5d86c36db3c321f63cd7
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2022-08-23 17:32:56 +0300

    Display user-names as a dot-list (without rendering cards)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 import api from "./utils/api";
+import Main from "./components/Main/Main";
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -14,11 +15,11 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(cards);
+  console.log(cards); //TODO: Delete this.
 
   return (
     <div className="App">
-      <span>Cards will be here soon</span>
+      <Main component={Main} cards={cards} />
     </div>
   );
 }
