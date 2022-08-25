5e33c11cc513b1059994db1d63b2537824fdd261
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2022-08-25 11:56:39 +0300

    Fix data loss in SessionStorage

--- src/components/UserDetail/UserDetail.jsx
+++ src/components/UserDetail/UserDetail.jsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router-dom";
 
 import "./UserDetail.css";
 
+// Store card information to keep data after user-page (/user/:id) reload
 function getCard(cards, id) {
   const data = cards.length
     ? cards.find((card) => card.id.toString() === id)
-    : sessionStorage.getItem("data");
+    : JSON.parse(sessionStorage.getItem("data"));
 
   // Use sessionStorage for reload "/users/:id" path
   sessionStorage.setItem("data", JSON.stringify(data));
@@ -17,10 +18,20 @@ function getCard(cards, id) {
 function UserDetail({ cards }) {
   const { id } = useParams();
   const user = getCard(cards, id);
+  console.log(user);
   return (
     <section className="detail">
       <div className="container">{id}</div>
       <div className="container">{user.name}</div>
+      <div className="container">{user.username}</div>
+      <div className="container">{user.email}</div>
+      <div className="container">{user.phone}</div>
+      <div className="container">{user.company.name}</div>
+      <div className="container">{user.website}</div>
+      <div className="container">{user.address.street}</div>
+      <div className="container">{user.address.suite}</div>
+      <div className="container">{user.address.city}</div>
+      <div className="container">{user.address.zipcode}</div>
     </section>
   );
 }
